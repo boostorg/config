c4e0b79c9d9c1e660d66808b09a59d57729a517d
jzmaddock <john@johnmaddock.co.uk>
2013-12-08 12:53:57 +0000

    Fix typo from https://github.com/boostorg/config/pull/2

--- include/boost/config/compiler/visualc.hpp
+++ include/boost/config/compiler/visualc.hpp
@@ -120,7 +120,7 @@
 #   define BOOST_HAS_NRVO
 #endif
 //
-// disable Win32 API's if compiler extentions are
+// disable Win32 API's if compiler extensions are
 // turned off:
 //
 #if !defined(_MSC_EXTENSIONS) && !defined(BOOST_DISABLE_WIN32)
