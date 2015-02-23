fada9f5ee2cd044ba0efd3c07bf9fc5070a98f81
jzmaddock <john@johnmaddock.co.uk>
2015-02-23 13:14:28 +0000

    Apply patch from https://svn.boost.org/trac/boost/ticket/6694

--- include/boost/cstdint.hpp
+++ include/boost/cstdint.hpp
@@ -492,7 +492,7 @@ INT#_C macros if they're not already defined (John Maddock).
 //  64-bit types + intmax_t and uintmax_t  ----------------------------------//
 #ifndef INT64_C
 #  if defined(BOOST_HAS_LONG_LONG) && \
-    (defined(ULLONG_MAX) || defined(ULONG_LONG_MAX) || defined(ULONGLONG_MAX) || defined(_LLONG_MAX))
+    (defined(ULLONG_MAX) || defined(ULONG_LONG_MAX) || defined(ULONGLONG_MAX) || defined(_ULLONG_MAX) || defined(_LLONG_MAX))
 
 #    if defined(__hpux)
         // HP-UX's value of ULONG_LONG_MAX is unusable in preprocessor expressions
@@ -501,7 +501,8 @@ INT#_C macros if they're not already defined (John Maddock).
 #    elif (defined(ULLONG_MAX) && ULLONG_MAX == 18446744073709551615ULL) ||  \
         (defined(ULONG_LONG_MAX) && ULONG_LONG_MAX == 18446744073709551615ULL) ||  \
         (defined(ULONGLONG_MAX) && ULONGLONG_MAX == 18446744073709551615ULL) || \
-        (defined(_LLONG_MAX) && _LLONG_MAX == 18446744073709551615ULL)
+        (defined(_ULLONG_MAX) && _ULLONG_MAX == 18446744073709551615ULL) || \ 
+        (defined(_LLONG_MAX) && _LLONG_MAX == 9223372036854775807LL) 
 
 #       define INT64_C(value) value##LL
 #       define UINT64_C(value) value##uLL
