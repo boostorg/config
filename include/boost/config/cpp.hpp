144cc76064502931c58f7fc24cf869ddd68f01c1
Edward Diener <eldiener@tropicsoft.com>
2015-06-04 10:01:37 -0400

    Added header file to bring in all the CPP header files at once.

---
+++ include/boost/config/cpp.hpp
@@ -0,0 +1,32 @@
+//  boost/config/cpp.hpp  ---------------------------------------------------//
+
+//  (C) Copyright Edward Diener 2015. 
+//  Use, modification and distribution are subject to the 
+//  Boost Software License, Version 1.0. (See accompanying file 
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#if !defined(BOOST_CONFIG_CPP_HPP)
+#define BOOST_CONFIG_CPP_HPP
+
+#include <boost/cpp/array.hpp>
+#include <boost/cpp/atomic.hpp>
+#include <boost/cpp/bind.hpp>
+#include <boost/cpp/chrono.hpp>
+#include <boost/cpp/function.hpp>
+#include <boost/cpp/hash.hpp>
+#include <boost/cpp/mem_fn.hpp>
+#include <boost/cpp/random.hpp>
+#include <boost/cpp/ratio.hpp>
+#include <boost/cpp/ref.hpp>
+#include <boost/cpp/regex.hpp>
+#include <boost/cpp/shared_ptr.hpp>
+#include <boost/cpp/thread.hpp>
+#include <boost/cpp/tuple.hpp>
+#include <boost/cpp/type_index.hpp>
+#include <boost/cpp/type_traits.hpp>
+#include <boost/cpp/unordered_map.hpp>
+#include <boost/cpp/unordered_multimap.hpp>
+#include <boost/cpp/unordered_multiset.hpp>
+#include <boost/cpp/unordered_set.hpp>
+
+#endif // !defined(BOOST_CONFIG_CPP_HPP)
