b2dd3d6c7065b819fb549fdb519537b29e3e8245
Edward Diener <eldiener@tropicsoft.com>
2015-06-04 12:06:42 -0400

    Corrected paths for global cpp header file.

--- include/boost/config/cpp.hpp
+++ include/boost/config/cpp.hpp
@@ -8,25 +8,25 @@
 #if !defined(BOOST_CONFIG_CPP_HPP)
 #define BOOST_CONFIG_CPP_HPP
 
-#include <boost/cpp/array.hpp>
-#include <boost/cpp/atomic.hpp>
-#include <boost/cpp/bind.hpp>
-#include <boost/cpp/chrono.hpp>
-#include <boost/cpp/function.hpp>
-#include <boost/cpp/hash.hpp>
-#include <boost/cpp/mem_fn.hpp>
-#include <boost/cpp/random.hpp>
-#include <boost/cpp/ratio.hpp>
-#include <boost/cpp/ref.hpp>
-#include <boost/cpp/regex.hpp>
-#include <boost/cpp/shared_ptr.hpp>
-#include <boost/cpp/thread.hpp>
-#include <boost/cpp/tuple.hpp>
-#include <boost/cpp/type_index.hpp>
-#include <boost/cpp/type_traits.hpp>
-#include <boost/cpp/unordered_map.hpp>
-#include <boost/cpp/unordered_multimap.hpp>
-#include <boost/cpp/unordered_multiset.hpp>
-#include <boost/cpp/unordered_set.hpp>
+#include <boost/config/cpp/array.hpp>
+#include <boost/config/cpp/atomic.hpp>
+#include <boost/config/cpp/bind.hpp>
+#include <boost/config/cpp/chrono.hpp>
+#include <boost/config/cpp/function.hpp>
+#include <boost/config/cpp/hash.hpp>
+#include <boost/config/cpp/mem_fn.hpp>
+#include <boost/config/cpp/random.hpp>
+#include <boost/config/cpp/ratio.hpp>
+#include <boost/config/cpp/ref.hpp>
+#include <boost/config/cpp/regex.hpp>
+#include <boost/config/cpp/shared_ptr.hpp>
+#include <boost/config/cpp/thread.hpp>
+#include <boost/config/cpp/tuple.hpp>
+#include <boost/config/cpp/type_index.hpp>
+#include <boost/config/cpp/type_traits.hpp>
+#include <boost/config/cpp/unordered_map.hpp>
+#include <boost/config/cpp/unordered_multimap.hpp>
+#include <boost/config/cpp/unordered_multiset.hpp>
+#include <boost/config/cpp/unordered_set.hpp>
 
 #endif // !defined(BOOST_CONFIG_CPP_HPP)
