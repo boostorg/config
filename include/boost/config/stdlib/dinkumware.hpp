553dcbec9600fce92aa94ca27e4e9c5b0f94b434
jzmaddock <john@johnmaddock.co.uk>
2016-12-15 10:21:26 +0000

    MSVC has no std::apply.

--- include/boost/config/stdlib/dinkumware.hpp
+++ include/boost/config/stdlib/dinkumware.hpp
@@ -163,8 +163,8 @@
 #endif
 
 // C++17 features
-#if !defined(_CPPLIB_VER) || (_CPPLIB_VER < 650)
 #  define BOOST_NO_CXX17_STD_APPLY
+#if !defined(_CPPLIB_VER) || (_CPPLIB_VER < 650)
 #  define BOOST_NO_CXX17_STD_INVOKE
 #endif
 
