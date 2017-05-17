219c351cb495aed4a993234d0d51da9927736f1e
Glen Fernandes <glen.fernandes@gmail.com>
2017-05-17 01:07:18 -0400

    Add BOOST_NO_CXX11_POINTER_TRAITS

--- test/config_test.cpp
+++ test/config_test.cpp
@@ -1,4 +1,4 @@
-//  This file was automatically generated on Mon Apr 17 18:35:54 2017
+//  This file was automatically generated on Wed May 17 01:29:40 2017
 //  by libs/config/tools/generate.cpp
 //  Copyright John Maddock 2002-4.
 //  Use, modification and distribution are subject to the 
@@ -236,6 +236,11 @@ namespace boost_no_cxx11_non_public_defaulted_functions = empty_boost;
 #include "boost_no_cxx11_numeric_limits.ipp"
 #else
 namespace boost_no_cxx11_numeric_limits = empty_boost;
+#endif
+#ifndef BOOST_NO_CXX11_POINTER_TRAITS
+#include "boost_no_cxx11_pointer_traits.ipp"
+#else
+namespace boost_no_cxx11_pointer_traits = empty_boost;
 #endif
 #ifndef BOOST_NO_CXX11_REF_QUALIFIERS
 #include "boost_no_cxx11_ref_qualifiers.ipp"
@@ -1519,6 +1524,11 @@ int main( int, char *[] )
    if(0 != boost_no_cxx11_numeric_limits::test())
    {
       std::cerr << "Failed test for BOOST_NO_CXX11_NUMERIC_LIMITS at: " << __FILE__ << ":" << __LINE__ << std::endl;
+      ++error_count;
+   }
+   if(0 != boost_no_cxx11_pointer_traits::test())
+   {
+      std::cerr << "Failed test for BOOST_NO_CXX11_POINTER_TRAITS at: " << __FILE__ << ":" << __LINE__ << std::endl;
       ++error_count;
    }
    if(0 != boost_no_cxx11_ref_qualifiers::test())
