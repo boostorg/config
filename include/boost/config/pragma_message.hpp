23327d6d0169c73ace8f7b91a40f03f4de4839fa
Peter Dimov <pdimov@pdimov.com>
2017-12-04 19:15:36 +0200

    Suppress BOOST_PRAGMA_MESSAGE messages when BOOST_DISABLE_PRAGMA_MESSAGE is defined

--- include/boost/config/pragma_message.hpp
+++ include/boost/config/pragma_message.hpp
@@ -16,12 +16,14 @@
 
 #include <boost/config/helper_macros.hpp>
 
-#if defined(__GNUC__)
-#define BOOST_PRAGMA_MESSAGE(x) _Pragma(BOOST_STRINGIZE(message(x)))
+#if defined(BOOST_DISABLE_PRAGMA_MESSAGE)
+# define BOOST_PRAGMA_MESSAGE(x)
+#elif defined(__GNUC__)
+# define BOOST_PRAGMA_MESSAGE(x) _Pragma(BOOST_STRINGIZE(message(x)))
 #elif defined(_MSC_VER)
-#define BOOST_PRAGMA_MESSAGE(x) __pragma(message(__FILE__ "(" BOOST_STRINGIZE(__LINE__) "): note: " x))
+# define BOOST_PRAGMA_MESSAGE(x) __pragma(message(__FILE__ "(" BOOST_STRINGIZE(__LINE__) "): note: " x))
 #else
-#define BOOST_PRAGMA_MESSAGE(x)
+# define BOOST_PRAGMA_MESSAGE(x)
 #endif
 
 #endif // BOOST_CONFIG_PRAGMA_MESSAGE_HPP_INCLUDED
