2ec8f0fc738be251cc8ce9a0700fbc4027f08ac9
Peter Dimov <pdimov@pdimov.com>
2018-01-19 05:01:39 +0200

    Add file/line to BOOST_PRAGMA_MESSAGE on Intel C++

--- include/boost/config/pragma_message.hpp
+++ include/boost/config/pragma_message.hpp
@@ -18,6 +18,8 @@
 
 #if defined(BOOST_DISABLE_PRAGMA_MESSAGE)
 # define BOOST_PRAGMA_MESSAGE(x)
+#elif defined(__INTEL_COMPILER)
+# define BOOST_PRAGMA_MESSAGE(x) __pragma(message(__FILE__ "(" BOOST_STRINGIZE(__LINE__) "): note: " x))
 #elif defined(__GNUC__)
 # define BOOST_PRAGMA_MESSAGE(x) _Pragma(BOOST_STRINGIZE(message(x)))
 #elif defined(_MSC_VER)
