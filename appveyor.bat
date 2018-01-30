0275380f326d3cda038d05437c794ae98318ac82
jzmaddock <john@johnmaddock.co.uk>
2018-01-30 17:58:24 +0000

    Appveyor: try to fix quoting in batch script.

--- appveyor.bat
+++ appveyor.bat
@@ -1,6 +1,12 @@
-IF NOT DEFINED CXXSTD (..\..\..\b2 config_info_travis_install "%ARGS%")
+IF NOT DEFINED CXXSTD (
+ECHO %ARGS:"=%
+..\..\..\b2 config_info_travis_install %ARGS:"=%
+config_info_travis
+del config_info_travis.exe
+)
 IF DEFINED CXXSTD FOR %%A IN (%CXXSTD%) DO (
-..\..\..\b2 -a -d2 config_info_travis_install %ARGS% cxxstd=%%A
+ECHO %ARGS:"=%
+..\..\..\b2 -a -d2 config_info_travis_install %ARGS:"=% cxxstd=%%A
 config_info_travis
 del config_info_travis.exe
 )
