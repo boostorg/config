fb9957101f3076aca0e94c396ccf3ff1ee3f74d3
jzmaddock <john@johnmaddock.co.uk>
2018-08-04 19:35:11 +0100

    Correct use of __has_include in suffix.hpp.

--- include/boost/config/detail/suffix.hpp
+++ include/boost/config/detail/suffix.hpp
@@ -1024,8 +1024,12 @@ namespace std{ using ::type_info; }
 #endif
 
 // This is a catch all case for obsolete compilers / std libs:
-#if !defined(__has_include) || !__has_include(<optional>)
+#if !defined(__has_include)
 #  define BOOST_NO_CXX17_HDR_OPTIONAL
+#else
+#if !__has_include(<optional>)
+#  define BOOST_NO_CXX17_HDR_OPTIONAL
+#endif
 #endif
 
 //
