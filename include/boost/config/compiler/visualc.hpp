d229f9be9670f6721e619583609d1e43207cf1ae
jzmaddock <john@johnmaddock.co.uk>
2018-09-15 08:22:00 +0100

    Latest msvc seems OK with C++14 constexpr now.

--- include/boost/config/compiler/visualc.hpp
+++ include/boost/config/compiler/visualc.hpp
@@ -236,9 +236,11 @@
 // Supported from msvc-15.5 onwards:
 #define BOOST_NO_CXX11_SFINAE_EXPR
 #endif
+#if (_MSC_VER < 1915) || (_MSVC_LANG < 201402)
 // C++ 14:
 // Still gives internal compiler error for msvc-15.5:
 #  define BOOST_NO_CXX14_CONSTEXPR
+#endif
 // C++ 17:
 #if (_MSC_VER < 1912) || (_MSVC_LANG < 201703)
 #define BOOST_NO_CXX17_INLINE_VARIABLES
