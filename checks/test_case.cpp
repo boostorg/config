d112744885c099d9011f8cf9a89bcf50521af6ca
jzmaddock <john@johnmaddock.co.uk>
2019-04-01 19:08:54 +0100

    Add BOOST_NO_CXX17_HDR_VARIANT. See https://github.com/boostorg/config/issues/264

--- checks/test_case.cpp
+++ checks/test_case.cpp
@@ -1,4 +1,4 @@
-//  This file was automatically generated on Mon Feb 11 18:07:32 2019
+//  This file was automatically generated on Mon Apr  1 19:04:05 2019
 //  by libs/config/tools/generate.cpp
 //  Copyright John Maddock 2002-4.
 //  Use, modification and distribution are subject to the 
@@ -524,6 +524,11 @@
 #ifdef TEST_BOOST_NO_CXX17_HDR_STRING_VIEW
 #  ifdef BOOST_NO_CXX17_HDR_STRING_VIEW
 #     error "Defect macro BOOST_NO_CXX17_HDR_STRING_VIEW is defined."
+#  endif
+#endif
+#ifdef TEST_BOOST_NO_CXX17_HDR_VARIANT
+#  ifdef BOOST_NO_CXX17_HDR_VARIANT
+#     error "Defect macro BOOST_NO_CXX17_HDR_VARIANT is defined."
 #  endif
 #endif
 #ifdef TEST_BOOST_NO_CXX17_IF_CONSTEXPR
