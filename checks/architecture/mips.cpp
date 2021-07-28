1a55d1d9c6d1cf7739645080bdd92fe903b4211e
Nikita Kniazev <nok.raven@gmail.com>
2021-07-28 21:06:54 +0300

    Rename mips1 to mips and alias mips1 to mips

--- checks/architecture/mips.cpp
+++ checks/architecture/mips.cpp
@@ -1,4 +1,4 @@
-// mips1.cpp
+// mips.cpp
 //
 // Copyright (c) 2012 Steven Watanabe
 //
@@ -7,5 +7,5 @@
 // http://www.boost.org/LICENSE_1_0.txt)
 
 #if !(defined(__mips) || defined(_MIPS_ISA_MIPS1) || defined(_R3000))
-#error "Not MIPS1"
+#error "Not MIPS"
 #endif
